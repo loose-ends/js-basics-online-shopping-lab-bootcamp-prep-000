d5c57189b80f6437fb0ae1a48a4fbd9e2be3ff3e
Tom Hynes <thomas.hynes@outlook.com>
2017-11-15 20:35:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ function addToCart(itemName) {
 }
 
 function viewCart() {
-  if (cart.length == 0) {console.log('Your shopping cart is empty.')}
+  if (Object.keys(cart).length == 0) {console.log('Your shopping cart is empty.')}
   else {
     var i = 0
     const list = []
-    while (i < cart.length-1) {
+    while (i < Object.keys(cart).length-1) {
       list.push(` ${Object.keys(cart)[i]} at $${Object.values(cart)[i]}`)
       i++
     }
-    list.push(` ${Object.keys(cart)[cart.length]} at $${Object.values(cart)[cart.length]}`)
+    list.push(` ${Object.keys(cart)[Object.keys(cart).length]} at $${Object.values(cart)[Object.keys(cart).length]}`)
     console.log(`In your cart, you have${list}.`)
   }
 }
