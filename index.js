c4fa177a16a8b050b98114fe55afca1a46192820
Tom Hynes <thomas.hynes@outlook.com>
2017-11-15 18:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ console.log(`${itemName} has been added to your cart.`)
+ var itemPrice = Math.floor(Math.random()*100)
+ cart.push({[itemName]:itemPrice})
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  var i = []
+  if (cart.length === 0) {console.log('Your shopping cart is empty.')}
+  else {
+    var list = []
+    for (i = 0; i < cart.length; i++) {
+      list.push('${cart.itemName[0]} at $${cart.itemPrice[1]}.') // need to understand object.keys? - split contents
+    }
+    console.log("In your cart, you have " + list)
+  }
 }
 
 function total() {
